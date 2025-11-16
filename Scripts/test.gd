3c9e37444e0541b140e3e84881b9fe92020086a5
Herovero <herovero0117@gmail.com>
2025-11-16 23:31:03 +0800

    disabled gravity for CharacterBody

--- Scripts/test.gd
+++ Scripts/test.gd
@@ -2,9 +2,7 @@ extends RigidBody2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-#var gravity = 400
-#var speed = 800
-@export var tilt_strength: float = 1500.0
+@export var tilt_strength: float = 3000.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,14 +13,11 @@ func _physics_process(delta):
 	var input_direction = Vector2.ZERO
 	
 	if OS.has_feature("mobile"):
-		print_debug("Mobile mode")
 		var sensor_data = Input.get_gravity()
-		
-		input_direction = Vector2(sensor_data.x, sensor_data.z)
+		input_direction = Vector2(sensor_data.x, sensor_data.y)
+		input_direction = input_direction / 9.8
 	else:
-		print_debug("PC mode")
 		input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-		pass
 	
 	var force = input_direction * tilt_strength
 	apply_central_force(force)
