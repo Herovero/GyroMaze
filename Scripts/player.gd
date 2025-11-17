40e63e2c319d40d0f036296060c3013fd5bffd53
Herovero <herovero0117@gmail.com>
2025-11-17 18:36:46 +0800

    Made a testing maze level with hole traps

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,6 +4,8 @@ extends RigidBody2D
 
 @export var tilt_strength: float = 5000.0
 
+var should_reset = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,3 +23,26 @@ func _process(delta):
 	
 	var force = input_direction * tilt_strength
 	apply_central_force(force)
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		reset_position()
+	elif event.is_action_pressed("ui_accept"):
+		reset_position()
+
+# 1. Trigger the flag
+func reset_position():
+	should_reset = true
+
+# 2. Handle the actual movement safely inside the physics loop
+func _integrate_forces(state):
+	if should_reset:
+		# Teleport the body
+		state.transform.origin = Vector2(0, 0)
+		
+		# Kill all momentum (stop it from flying)
+		state.linear_velocity = Vector2.ZERO
+		state.angular_velocity = 0
+		
+		# Turn the flag off so we don't get stuck at (0,0)
+		should_reset = false
--- Scripts/player2.gd
+++ Scripts/player2.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
+@onready var player: CharacterBody2D = $"."
+
 var speed = 100.0
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("restart"):
+		print("wdqq")
+		
 	var accelerationRate = Input.get_accelerometer()
 	
 	# 1. Set the built-in 'velocity' property
@@ -10,3 +15,7 @@ func _physics_process(delta):
 	
 	# 2. Call move_and_slide() with NO arguments
 	move_and_slide()
+
+func reset_position():
+	print("wdq")
+	global_position = Vector2(0, 0)
